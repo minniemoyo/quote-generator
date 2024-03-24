34310ce654f06107b00caf8b36d0dfb86bc28d6d
minniemoyo <hellominniemoyo@gmail.com>
2024-03-24 18:16:50 +1100

    Added basic javascript functionality to the page

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+function displayQuote(event) {
+  event.preventDefault();
+  let quoteOutput = document.querySelector("#quoteOutput");
+  quoteOutput.innerHTML = "This is where the quote will go!";
+}
 
+let searchInput = document.querySelector("#form-input");
+searchInput.addEventListener("submit", displayQuote);
